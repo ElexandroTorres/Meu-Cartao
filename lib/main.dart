e87fee6e7a625b803685ef4002d0cd25692e3ec0
Elexandro Torres <elexandro.torres@gmail.com>
2021-11-05 09:21:03 -0300

    Infos with cards and an image are add

--- lib/main.dart
+++ lib/main.dart
@@ -8,59 +8,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: NetworkImage(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPmZUEXxUASGKMSX1P8FQDpPYV5dKZ_RP-Cw&usqp=CAU',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPmZUEXxUASGKMSX1P8FQDpPYV5dKZ_RP-Cw&usqp=CAU',
+                ),
               ),
-            ),
-            Text(
-              'Elexandro Torres',
-              style: TextStyle(
-                fontSize: 35.0,
+              Text(
+                'Elexandro Torres',
+                style: TextStyle(
+                  fontSize: 35.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Dancing',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Josefin',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Dancing',
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.teal.shade100,
-                letterSpacing: 2.5,
-                fontFamily: 'Josefin',
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  Icon(
+                child: ListTile(
+                  leading: Icon(
                     Icons.add_shopping_cart,
+                    color: Colors.black,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     '+55 84 988322989',
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'elexandro.torres@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
+
+/*
+ Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_shopping_cart,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+55 84 988322989',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+*/
